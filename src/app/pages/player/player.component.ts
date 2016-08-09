9fe1602e4b92c69b6044dd3c0610e3cb305f00fb
habib kazemi <hkazemi@cse.shirazu.ac.ir>
2016-08-10 00:53:16 +0430

    save position in demo

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -3,7 +3,7 @@ import {Component,OnInit} from "@angular/core";
 import application = require("application");
 import {VLCComponent} from "nativescript-ng2-vlc-player/nativescript-ng2-vlc-player";
 import {ActivatedRoute} from "@angular/router";
-
+import appSettings = require("application-settings");
 @Component({
     selector: "player",
     templateUrl: "pages/player/player.html",
@@ -17,6 +17,7 @@ export class playerPage implements OnInit{
     vlcAction;
     private sub:any;
     public path:string;
+    public position:number = 0;
     eventCallback = {
         eventHardwareAccelerationError:function(){
           console.log("eventHardwareAccelerationError");
@@ -44,22 +45,30 @@ export class playerPage implements OnInit{
        this.path = params['path'];
      });
 
+     this.position = appSettings.getNumber(this.path,0);
+
       application.android.off(application.AndroidApplication.activityPausedEvent)
       application.android.on(application.AndroidApplication.activityPausedEvent,
         function (args: application.AndroidActivityEventData) {
           this.vlc.stopPlayback();
+          this.position = this.vlc.lastPosition;
+          this.save();
       },this);
 
       application.android.off(application.AndroidApplication.activityDestroyedEvent);
       application.android.on(application.AndroidApplication.activityDestroyedEvent,
         function (args: application.AndroidActivityEventData) {
           this.vlc.stopPlayback();
+          this.position = this.vlc.lastPosition;
+          this.save();
         },this);
 
       application.android.off(application.AndroidApplication.activityBackPressedEvent);
       application.android.on(application.AndroidApplication.activityBackPressedEvent,
         function (args) {
           this.vlc.stopPlayback();
+          this.position = this.vlc.lastPosition;
+          this.save();
       },this);
     }
 
@@ -71,6 +80,10 @@ export class playerPage implements OnInit{
       console.log('maxVolume');
       console.log(obj.maxVolume);
 
+    }
+
+    public save(){
+      appSettings.setNumber(this.path, this.position);
     }
 
 }
